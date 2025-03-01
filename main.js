d27d586c3ea68bc485bd998411b7aeff5f412a3d
antoniodicillo <125458796+antoniodicillo@users.noreply.github.com>
2025-03-01 17:50:09 -0300

    pf conserta github pages

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class BootScene extends Phaser.Scene {
     });
   }
   preload() {
-    this.load.image("background", "assets/Background.png)
+    this.load.image("background", "assets/Background.png")
   }
 
   create() {
